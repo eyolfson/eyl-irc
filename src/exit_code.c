385d2917466750208d0cf9fa05fd52cda214db5a
Jon Eyolfson <jon@eyl.io>
2016-01-31 12:55:00 -0500

    Rearrange exit_code declarations

--- src/exit_code.c
+++ src/exit_code.c
@@ -5,10 +5,6 @@
 static pthread_mutex_t exit_code_lock = PTHREAD_MUTEX_INITIALIZER;
 static uint8_t exit_code = 0;
 
-uint8_t get_exit_code() {
-	return exit_code;
-}
-
 void set_exit_code(uint8_t value)
 {
 	pthread_mutex_lock(&exit_code_lock);
@@ -25,4 +21,8 @@ bool is_exiting()
 	}
 	pthread_mutex_unlock(&exit_code_lock);
 	return is_exit_code_set;
+}
+
+uint8_t get_exit_code() {
+	return exit_code;
 }
--- src/exit_code.h
+++ src/exit_code.h
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-uint8_t get_exit_code();
 void set_exit_code(uint8_t value);
-
 bool is_exiting();
+uint8_t get_exit_code();
